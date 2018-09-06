3635ca826124a0621a38a88591545925d711a277
neighborpil <neighborpil@naver.com>
2018-09-07 08:04:09 +0900

    ch6 p224

---
+++ Part1_Basics/CH05/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CH05.Startup))]
+namespace CH05
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
